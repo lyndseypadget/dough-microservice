d3f7caa19b815a583792212df012cfae44ca035a
Lyndsey Padget <lpadget@freightview.com>
2017-08-03 09:16:01 -0500

    Tests

--- test/services/yeast.js
+++ test/services/yeast.js
@@ -1,9 +1,31 @@
 var assert = require('assert');
+var moment = require('moment');
+var yeastService = require('../../src/services/yeast');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Proof', function() {
+
+  let now = moment();
+
+  it('should return an error if you haven\'t waited long enough', function(done) {
+    let yeastMixture = {
+      beginTime: now.clone().subtract(3, 'seconds')
+    };
+    yeastService.testProof(yeastMixture, function(err) {
+      assert.ok(err);
+      assert.ok(/Proof is not ready/i.test(err));
+      done();
+    });
+  });
+
+  it('should NOT return an error if you have waited long enough', function(done) {
+    let yeastMixture = {
+      beginTime: now.clone().subtract(7, 'seconds')
+    };
+    yeastService.testProof(yeastMixture, function(err, updatedYeastMixture) {
+      assert.ifError(err);
+      assert.ok(updatedYeastMixture.endTime);
+      assert.ok(updatedYeastMixture.hasFrothyBubbles);
+      done();
     });
   });
 });
